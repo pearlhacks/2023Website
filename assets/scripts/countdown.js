e563d756381ee3e4e446a98c948bf310bc7dac75
Selena Li <selenali@live.unc.edu>
2021-10-04 10:56:03 -0400

    added background img

--- assets/scripts/countdown.js
+++ assets/scripts/countdown.js
@@ -1,6 +1,6 @@
 //https://www.w3schools.com/howto/howto_js_countdown.asp
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+var countDownDate = new Date("Feb 18, 2022 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -18,7 +18,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdown").innerHTML ="<i class='fas fa-stopwatch text-white mr-3' aria-hidden='true'></i>"+ days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
